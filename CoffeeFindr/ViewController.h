d194093660cdf99c5935738acd20c664e073045c
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-06 20:52:14 -0800

    Initial Commit

---
+++ CoffeeFindr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoffeeFindr
+//
+//  Created by Ronald Hernandez on 3/6/15.
+//  Copyright (c) 2015 Ronald Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
